de570cc9ce4f809ecdbebdd8adca4107603e9c54
Devin Brown <devin.brown@c02phedbfvh3.ad.insidesales.com>
2018-11-28 15:19:44 -0700

    app setup

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import dash_core_components as dcc
-import dash_html_componenents as html
-import dash_table_experiments as dtable
-import dash_dependencies
-from dash.dependenciest import Input, Output, State
+import dash_html_components as html
+#import dash_table_experiments as dtable
+#import dash_dependencies
+#from dash.dependenciest import Input, Output, State
 import plotly
 import seaborn
 import numpy
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv')
+
+
+#Create app layout
+app = dash.Dash(__name__)
+server = app.server
+app.layout
+
 
 
