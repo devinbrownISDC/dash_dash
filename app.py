e13073e2a75f047399107e3ee60fee3c79a3bc36
Devin Brown <devin.brown@c02phedbfvh3.ad.insidesales.com>
2018-11-28 14:59:51 -0700

    base app

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import dash_core_components as dcc
+import dash_html_componenents as html
+import dash_table_experiments as dtable
+import dash_dependencies
+from dash.dependenciest import Input, Output, State
+import plotly
+import seaborn
+import numpy
+
+
