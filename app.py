0ebb6f124fae2fd4fcfd4eef09fa115595b0fe55
Devin Brown <devin.brown@c02phedbfvh3.ad.insidesales.com>
2018-12-03 15:47:00 -0700

    table edits

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash
 import dash_html_components as html
 import plotly.graph_objs as go
-#import dash_table_experiments as dtable
+import dash_table
 #import dash_dependencies
 #from dash.dependenciest import Input, Output, State
 import plotly
@@ -19,10 +19,22 @@ app = dash.Dash(__name__)
 server = app.server
 app.layout = html.Div(children=[
 html.H1('Hello Dash'),
-dcc.Graph(id='mapbox',
-          figure = { 'data': [go.Bar(x=df['Postal'],y=df['Population'])]}
-          )
-])
+
+#basic graph syntax
+dcc.Graph(
+    id='mapbox',
+    figure = {'data': [go.Bar(x=df['Postal'],y=df['Population'])]}
+),
+
+#basic table syntax
+dash_table.DataTable(
+    id='table',
+    columns = [{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict("rows")
+    )
+]
+
+)
 
 
 
